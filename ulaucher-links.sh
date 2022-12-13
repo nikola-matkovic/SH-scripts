d460214753d8d249564a3d9ca8817c39da48f768
Nikola Matkovic <60906422+anonymousvegan@users.noreply.github.com>
2022-12-13 21:55:42 +0100

    Add ulaucher install scripsts

---
+++ ulaucher-links.sh
@@ -0,0 +1,43 @@
+# DEB packages
+# sudo apt install python python3-pip wget git -y fd-find
+# sudo add-apt-repository ppa:agornostal/ulauncher && sudo apt update && sudo apt install ulauncher
+
+fedora packages
+sudo dnf install python3 python3-pip wget git fd-find
+sudo dnf install ulauncher
+
+pip install fuzzywuzzy
+pip install uuid
+pip install pyperclip
+wget git.io/trans
+chmod +x ./trans
+pip3 install pwgen
+pip install regex
+pip install Pint simpleeval parsedatetime pytz
+pip install babel
+pip install gkeepapi
+pip3 install lorem
+
+cd ./local/share/ulauncher/extensions
+
+https://github.com/rootwork/ulauncher-symbol
+https://github.com/NastuzziSamy/ulauncher-youtube-search
+https://github.com/plibither8/ulauncher-vscode-recent
+https://github.com/Quitten/ulauncher-notes
+https://github.com/kleber-swf/ulauncher-turn-off-screen
+https://github.com/NastuzziSamy/ulauncher-translate
+https://github.com/elken/Terminal-Runner
+https://github.com/Ulauncher/ulauncher-emoji
+https://github.com/brpaz/ulauncher-pwgen
+https://github.com/brpaz/ulauncher-file-search
+https://github.com/sergius02/ulauncher-colorconverter
+https://github.com/fsevenm/ulauncher-wordc
+https://github.com/jmakhack/ulauncher-neofetch-runner
+https://github.com/tchar/ulauncher-albert-calculate-anything
+https://github.com/DevKleber/ulauncher-open-link
+https://github.com/isacikgoz/ukill
+https://github.com/ybotspawn/Keep-ULauncher
+https://github.com/luasenvy/ulauncher-node
+https://github.com/brpaz/ulauncher-lipsum
+https://github.com/Glovecc/ulauncher-github-search
+https://github.com/skykooler/ulauncher-conversion
